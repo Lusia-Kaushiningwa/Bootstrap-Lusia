4f4dbae58ff6aaf60a43753d499b103ddf6ec202
Lusia Kaushiningwa <115229398+Lusia-Kaushiningwa@users.noreply.github.com>
2026-02-26 21:51:30 +0200

    Creating folders

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+function checkHuman() {
+  let answer = prompt("What is 3 + 5 ?");
+  
+  if (answer == 8) {
+    alert("Correct! You are human 😊");
+  } else {
+    alert("Wrong answer. Try again.");
+  }
+}
