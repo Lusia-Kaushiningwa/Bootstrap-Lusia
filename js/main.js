66af493ade9d52999e0caedfd0a38d05bae24c88
Lusia Kaushiningwa <115229398+Lusia-Kaushiningwa@users.noreply.github.com>
2026-02-28 08:28:08 +0200

    deleting js codes
    the codes in Js are not needed for now.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,1 @@
 
-function checkHuman() {
-  let answer = prompt("What is 3 + 5 ?");
-  
-  if (answer == 8) {
-    alert("Correct! You are human 😊");
-  } else {
-    alert("Wrong answer. Try again.");
-  }
-}
